11b719ea7b5797cd59f0eed7fbfac903670068cf
Guilherme Veiga Pedromilo <guipedromilo@gmail.com>
2024-03-08 12:52:47 -0300

    update

--- src/components/LoginImageBackground.js
+++ src/components/LoginImageBackground.js
@@ -2,30 +2,18 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TextInput, ImageBackground, Button } from 'react-native';
 import styles from '../css/stylelogin'; // Importando styles, do folder CSS
-import LoginText from './LoginText'; // Importando componente LoginText
-import LoginButton from './LoginButton'; // Importando componente LoginButton
-import LoginInput from './LoginInput'; // Importando componente LoginInput
-import LoginImage from './LoginImage'; // Importando componente LoginImage
 
-function LoginImageBackground() {
+
+function LoginImageBackground({children}) {
 
     return (
-        <View style={{flex: 1, }} >
             <ImageBackground
                 style={styles.body}
                 source={require('../assets/images/fundologin.jpg')}>
-                    <View style={styles.container}>
-                <View style={styles.submenu}>
-                    <LoginText /> {/* Renderizando componente LoginText */}
-                    <View style={{ gap: 10, }} >
-                        <LoginInput /> {/* Renderizando componente LoginInput */}
-                        <LoginButton />  {/* Renderizando componente LoginButton */}
-                    </View>
-                </View>
-                <LoginImage /> {/* Renderizando componente LoginImage */}
-                </View> 
-            </ImageBackground> 
-        </View>
+                {children}
+                </ImageBackground>
     );
 }
+
+
 export default LoginImageBackground;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { View,  } from 'react-native';
+import styles from '../css/stylelogin';
 import LoginImageBackground from '../components/LoginImageBackground'; //Aqui está importando o componente LoginImageBackground 
+import LoginText from '../components/LoginText'; // Importando componente LoginText
+import LoginButton from '../components/LoginButton'; // Importando componente LoginButton
+import LoginInput from '../components/LoginInput'; // Importando componente LoginInput
+import LoginImage from '../components/LoginImage'; // Importando componente LoginImage
 
 function Login() { // Alteração aqui: export default function Login()
 
   return (
-        <LoginImageBackground /> //Aqui está renderizando o componente LoginImageBackground na página Login
+        <LoginImageBackground>{/*Aqui está renderizando o componente LoginImageBackground na página Login*/}
+        <View style={styles.container}>
+        <View style={styles.submenu}>
+            <LoginText /> {/* Renderizando componente LoginText */}
+            <View style={{ gap: 10, }} >
+                <LoginInput /> {/* Renderizando componente LoginInput */}
+                <LoginButton />  {/* Renderizando componente LoginButton */}
+            </View>
+        </View>
+        <LoginImage /> {/* Renderizando componente LoginImage */}
+        </View> 
+        </LoginImageBackground>
   );
 }
 
